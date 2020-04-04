b92408b420a4654877bc89949ce1b800cc5a0e3e
Etash Guha <etashguha@Etashs-MacBook-Pro.local>
2020-04-04 02:46:00 -0400

    redone token

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'verify.html'),
+    pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
